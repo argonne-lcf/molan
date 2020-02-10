9345a1219ed06a6721ee43e365497c55df10384a
pythonpanda2 <46133994+pythonpanda2@users.noreply.github.com>
2020-02-09 21:02:39 -0600

    Update melting_run_nonGDB.sh

--- code/melting_predict_Spark_RF/scripts/melting_run_nonGDB.sh
+++ code/melting_predict_Spark_RF/scripts/melting_run_nonGDB.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-./submit-spark.sh -A datascience -t 480  -n 8  -q  pubnet   Predict_Melting.py  -p  /path/to/Melting/dataset/   -db  All    --nodes 8  --cores 12 --npart 4  -m  RF  -d  Morgan2DCMSE    -b No  -cv  No 
+./submit-spark.sh -A XXX -t 480  -n 8  -q  pubnet   Predict_Melting.py  -p  /path/to/Melting/dataset/   -db  All    --nodes 8  --cores 12 --npart 4  -m  RF  -d  Morgan2DCMSE    -b No  -cv  No 
